879304ede160d31e16d4092681c272097296fc8e
Chris Julian <chris@zero1many.net>
2017-09-17 02:01:30 -0400

    Now resolving the webservers using dns container

--- dns/entry.sh
+++ dns/entry.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -e
+set -u
+
 echo "Starting DNS server with:"
 echo "Legit IP: " $LEGIT_IP
 echo "Legit name: " $LEGIT_DNSNAME
 echo "Evil IP: " $EVIL_IP
 echo "Evil name: " $EVIL_DNSNAME
 
+echo -e "$LEGIT_IP\t$LEGIT_DNSNAME" >> /etc/hosts
+echo -e "$EVIL_IP\t$EVIL_DNSNAME" >> /etc/hosts
 exec dnsmasq -k -d
+# exec /bin/bash
